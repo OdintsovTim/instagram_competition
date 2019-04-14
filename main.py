dd6c1579e63fcd635a2d648a2a55313b67b67213
Odintsov Timur <timodin@yandex.ru>
2019-04-14 17:43:48 +0300

    Revert "Revert "added print""
    This reverts commit 14e8a65315f63685eabc9368ae79ed1c6f16bba0.

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
                 comment['user']['username'],
             ))
 
-    fulfilled_condidions_users = set(fulfilled_condidions_users)
+    print(fulfilled_condidions_users = set(fulfilled_condidions_users))
 
 
 if __name__ == "__main__":
